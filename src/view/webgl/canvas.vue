9b9618552821bf8a6582b0df0e9dc5497415b48b
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2023-06-01 17:08:54 +0800

    feat: webgl

--- src/view/webgl/canvas.vue
+++ src/view/webgl/canvas.vue
@@ -1,23 +1,18 @@
 <template>
-  <canvas id="canvas" width="400" height="400" ref="canvasRef">
-    此浏览器不支持 canvas
-  </canvas>
+  <canvas id="canvas" width="400" height="400" ref="canvasRef"> 此浏览器不支持 canvas </canvas>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref } from 'vue';
 const canvasRef = ref(null);
 
-onMounted(()=>{
-  const canvas = canvasRef.value
-  const ctx  = canvas.getContext('2d')
+onMounted(() => {
+  const canvas = canvasRef.value;
+  const ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = 'red'
-  ctx.fillRect(10,10,100,100)
-})
+  ctx.fillStyle = 'red';
+  ctx.fillRect(10, 10, 100, 100);
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
-
+<style lang="scss" scoped></style>
--- src/view/webgl/webgl.vue
+++ src/view/webgl/webgl.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>
-
-  </div>
+  <canvas id="canvas" width="400" height="400" ref="canvasRef"> 此浏览器不支持 canvas </canvas>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+const canvasRef = ref(null);
 
+onMounted(() => {
+  const canvas = canvasRef.value;
+  const gl = canvas.getContext('webgl');
+
+  gl.clearColor(1.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
-
+<style lang="scss" scoped></style>
