085f8fd0dfc6edc015bdaa51f0b25e547ef5c894
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2023-06-12 11:54:43 +0800

    feat: threejs 基本使用

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
 
-import baseRouters from "@/router/module/base";
-import webglRouters from "@/router/module/webgl.js";
+import baseRouters from '@/router/module/base'
+import webglRouters from '@/router/module/webgl.js'
+import threeJsRouters from '@/router/module/three'
 
-const routes = [...baseRouters,...webglRouters];
+const routes = [...baseRouters, ...webglRouters, ...threeJsRouters]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() {
     return {
-      el: "#app",
+      el: '#app',
       top: 0,
-      behavior: "smooth",
-    };
+      behavior: 'smooth',
+    }
   },
-});
+})
 
-export default router;
+export default router
--- src/view/threejs/1.简单实现隐形、雾化效果/1.渲染三维对象/index.vue
+++ src/view/threejs/1.简单实现隐形、雾化效果/1.渲染三维对象/index.vue
@@ -1,13 +1,71 @@
 <template>
-  <canvas id="canvas" width="400" height="400" ref="canvasRef"> 此浏览器不支持 canvas </canvas>
+  <div></div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
-const canvasRef = ref(null)
+import { onMounted } from 'vue'
+import * as THREE from 'three'
 
 onMounted(() => {
-  const canvas = canvasRef.value
+  const app = document.querySelector('#app')
+  const canvasDom = document.querySelector('#canvas')
+
+  // 创建一个场景
+  const scene = new THREE.Scene()
+
+  // 创建相机，视点，设置相机位置
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+  camera.position.set(0, 0, 20)
+
+  // 创建一个渲染器
+  const renderer = new THREE.WebGLRenderer()
+
+  // 设置渲染尺寸
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  if (!canvasDom) {
+    renderer.domElement.id = 'canvas'
+    app.appendChild(renderer.domElement)
+  }
+
+  // 创建立方体
+  const cubeGeometry = new THREE.BoxGeometry(2, 2, 2)
+
+  // 创建材质
+  const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffff00,
+    // 是否以线框模式绘制
+    wireframe: false,
+  })
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
+  const sphereGeometry = new THREE.SphereGeometry(1, 10, 10)
+  const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffff00,
+    // 是否以线框模式绘制
+    wireframe: false,
+  })
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+  sphere.position.x = 3
+  sphere.position.y = 3
+
+  // 添加到场景
+  scene.add(cube)
+  scene.add(sphere)
+
+  const animation = () => {
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    sphere.rotation.x += 0.01
+    sphere.rotation.y += 0.01
+
+    // 渲染
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(animation)
+  }
+
+  animation()
 })
 </script>
 
