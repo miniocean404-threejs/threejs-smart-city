d8ecd2a09a12ae949a124cd7973ff1ecfcbc627e
MiniOcean404 <1037306928@qq.com>
2023-07-01 04:26:26 +0800

    feat: prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   // 一行最多 120 字符
-  printWidth: 120,
+  printWidth: 160,
   // 使用 2 个空格缩进
   tabWidth: 2,
   // 不使用缩进符，而使用空格
@@ -9,10 +9,10 @@ module.exports = {
   semi: false,
   // 使用单引号
   singleQuote: true,
+  // jsx 使用单引号
+  jsxSingleQuote: true,
   // 对象的 key 仅在必要时用引号
   quoteProps: 'as-needed',
-  // jsx 不使用单引号，而使用双引号
-  jsxSingleQuote: false,
   // 末尾需要有逗号
   trailingComma: 'all',
   // 大括号内的首尾需要空格
@@ -21,6 +21,19 @@ module.exports = {
   jsxBracketSameLine: false,
   // 箭头函数，只有一个参数的时候，也需要括号
   arrowParens: 'always',
+  // html, vue, jsx 中每个属性占一行
+  singleAttributePerLine: false,
+  // 根据显示样式决定 html 要不要折行
+  htmlWhitespaceSensitivity: 'css',
+  // 换行符使用 lf
+  endOfLine: 'lf',
+  // 使用默认的折行标准
+  proseWrap: 'preserve',
+  // vue 文件中的 script 和 style 内不用缩进
+  vueIndentScriptAndStyle: false,
+  // 格式化嵌入的内容
+  embeddedLanguageFormatting: 'auto',
+
   // 每个文件格式化的范围是文件的全部内容
   rangeStart: 0,
   rangeEnd: Infinity,
@@ -28,16 +41,4 @@ module.exports = {
   requirePragma: false,
   // 不需要自动在文件开头插入 @prettier
   insertPragma: false,
-  // 使用默认的折行标准
-  proseWrap: 'preserve',
-  // 根据显示样式决定 html 要不要折行
-  htmlWhitespaceSensitivity: 'css',
-  // vue 文件中的 script 和 style 内不用缩进
-  vueIndentScriptAndStyle: false,
-  // 换行符使用 lf
-  endOfLine: 'lf',
-  // 格式化嵌入的内容
-  embeddedLanguageFormatting: 'auto',
-  // html, vue, jsx 中每个属性占一行
-  singleAttributePerLine: true,
 }
